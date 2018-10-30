4dcba469d497fc128dfa95c514dfc6fc19e62b60
Hugo Tamayo <doctamayot@hotmail.com>
2018-10-29 22:35:40 -0500

    implementando el reto de listar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artist.create(name: "Foo", image_url: "https://img.peru21.pe/files/article_content_ec_fotos/uploads/2018/09/04/5b8e96b862d77.jpeg")
+Artist.create(name: "Guns", image_url: "https://elpais.com/cultura/imagenes/2016/01/05/actualidad/1451996412_919425_1452000859_noticia_fotograma.jpg")
+Album.create(name: "hugo", image_url: "https://img.peru", released_at:"1991", artist_id: 1)
+Album.create(name: "mary", image_url: "https://img.peru", released_at:"1994", artist_id: 2)
